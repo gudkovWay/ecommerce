5d9dfef9a0685b1cf1f44aca0dca99853022e620
gudkovWay <gudkoviurii@gmail.com>
2023-12-10 19:51:53 +0000

    no scroll body if modals open

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -20,7 +20,7 @@ export default function Home() {
     isSignInOpen || isSignUpOpen
       ? document.body.classList.add("no-scroll")
       : document.body.classList.remove("no-scroll");
-  }, []);
+  }, [isSignInOpen, isSignUpOpen]);
 
   return (
     <>
